87d618f7b31dd72554c1564371b9c81d4dfe9f8f
Daniel Emery <danielremery@gmail.com>
2023-05-22 08:15:29 +0200

    Add nix development flake and match version

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Keys Development Environment";
+
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
+
+  outputs = { self, nixpkgs }:
+    let
+      inherit (nixpkgs.lib) genAttrs;
+      supportedSystems = [
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      forAllSystems = f: genAttrs supportedSystems (system: f system);
+    in {
+      devShells = forAllSystems (system:
+        let pkgs = import nixpkgs { inherit system; };
+        in {
+          default = pkgs.mkShell {
+            nativeBuildInputs = [ pkgs.bashInteractive ];
+            buildInputs = with pkgs; [
+              deno
+            ];
+          };
+        });
+    };
+}
