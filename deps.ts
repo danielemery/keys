7898a45b1c5e88fed47d89c3c27b68dc4880156e
Daniel Emery <danielremery@gmail.com>
2023-10-20 07:15:38 +0200

    Bump deno and package versions (#13)
    - Upgrade to latest nix unstable version of deno (v1.37.1)
    - Upgrade to latest deno std library (0.204.0)
      - Migrate to new standard Deno.serve for main server (serve now deprecated)
      - Fix StatusText breaking changes (changed from a Map to a Record)

--- deps.ts
+++ deps.ts
@@ -1,5 +1,4 @@
-export { serve } from "https://deno.land/std@0.117.0/http/server.ts";
 export {
   Status,
   STATUS_TEXT,
-} from "https://deno.land/std@0.117.0/http/http_status.ts";
+} from "https://deno.land/std@0.204.0/http/http_status.ts";
