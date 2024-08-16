b3e7ffc90a54730f1ce6bd3758d8227b8053650d
Daniel Emery <danielremery@gmail.com>
2024-08-16 08:08:27 +0200

    #51 Load PGP keys from config file along with ssh keys on application start

--- src/load_config.ts
+++ src/load_config.ts
@@ -6,7 +6,13 @@ export default async function loadConfig(path: string) {
   const contents = await loadFileContents(path);
   try {
     const config = parse(contents);
-    return configSchema.parse(config);
+    const result = configSchema.parse(config);
+    console.log(
+      `Successfully loaded ${result["ssh-keys"].length} ssh public keys and ${
+        result["pgp-keys"].length
+      } pgp keys from ${path}.`,
+    );
+    return result;
   } catch (err) {
     console.error(`Failed to parse config file at path: ${path}`);
     throw err;
@@ -22,7 +28,10 @@ const configSchema = z.object({
       tags: z.array(z.string()).optional().default([]),
     }),
   ),
+  "pgp-keys": z.array(z.object({ name: z.string(), key: z.string() }))
+    .optional().default([]),
 });
 
 export type Config = z.infer<typeof configSchema>;
 export type PublicSSHKey = z.infer<typeof configSchema>["ssh-keys"][number];
+export type PGPKey = z.infer<typeof configSchema>["pgp-keys"][number];
