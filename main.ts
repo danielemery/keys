591f0a9e9ad86e77608151041df057f686706b66
Daniel Emery <danielremery@gmail.com>
2024-08-16 08:54:02 +0200

    #41 Add new pgp key routes based on the loaded keys

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ import { filterIncludesKey, parseParameters } from "./src/filter.ts";
 import { parseEnvironmentVariables } from "./src/environment.ts";
 import { Sentry } from "./deps.ts";
 import loadConfig from "./src/load_config.ts";
+import { getPGPTarget, servePGPKey, servePGPKeyList } from "./src/serve_pgp.ts";
 
 const environment = parseEnvironmentVariables(Deno.env.toObject());
 
@@ -15,14 +16,20 @@ if (environment.SENTRY_DSN) {
   });
 }
 
-const { "ssh-keys": keys } = await loadConfig(environment.CONFIG_PATH);
+const { "ssh-keys": sshKeys, "pgp-keys": pgpKeys } = await loadConfig(
+  environment.CONFIG_PATH,
+);
 
 start(
   environment.PORT,
   {
     filterIncludesKey,
     parseParameters,
-    keys,
+    getPGPTarget,
+    servePGPKey,
+    servePGPKeyList,
+    sshKeys,
+    pgpKeys,
   },
   environment.KEYS_VERSION,
 );
