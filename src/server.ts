61ea8fe70a1088c8df898c638fc956a5812e4443
Daniel Emery <danielremery@gmail.com>
2024-08-16 08:54:02 +0200

    #41 Add new pgp key routes based on the loaded keys

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import { STATUS_CODE, STATUS_TEXT } from "@std/http";
 import { filterIncludesKey, parseParameters } from "./filter.ts";
 import { PublicSSHKey } from "./load_config.ts";
+import { PGPKey } from "./load_config.ts";
+import { getPGPTarget, servePGPKey, servePGPKeyList } from "./serve_pgp.ts";
 
 /**
  * The dependencies required by the server.
@@ -9,12 +11,15 @@ import { PublicSSHKey } from "./load_config.ts";
 export interface ServerDependencies {
   filterIncludesKey: typeof filterIncludesKey;
   parseParameters: typeof parseParameters;
-  keys: PublicSSHKey[];
+  getPGPTarget: typeof getPGPTarget;
+  servePGPKey: typeof servePGPKey;
+  servePGPKeyList: typeof servePGPKeyList;
+  sshKeys: PublicSSHKey[];
+  pgpKeys: PGPKey[];
 }
 
 /**
- * Start a simple http server listening on the provided port that listens on
- * the provided port and provides authorized keys based on query string filter
+ * Start a simple http server that listens on the provided port and provides authorized keys based on query string filter
  * parameters.
  * @param port The port to listen on.
  */
@@ -30,22 +35,39 @@ export default function start(
   });
 }
 
-const validKeysRoutes = [
+const validSSHKeyRoutes = [
   "/keys",
   "/keys/",
   "/authorized_keys",
   "/authorized_keys/",
 ];
+const validPGPKeyRoutes = ["/pgp", "/pgp/"];
 
 export function handleRequest(
   req: Request,
   dependencies: ServerDependencies,
   version: string,
 ) {
+  const { servePGPKeyList, getPGPTarget, servePGPKey } = dependencies;
   try {
     const url = new URL(req.url);
+
+    /** If the url is /pgp return the list of loaded pgp keys. */
+    if (validPGPKeyRoutes.includes(url.pathname)) {
+      return servePGPKeyList(version, dependencies);
+    }
+
+    /**
+     * If the url is /pgp/${some_key} then return the pgp key body.
+     * If a file extension is included in the url, content disposition headers are set to indicate download is preferred.
+     */
+    const pgpKeyTarget = getPGPTarget(url.pathname);
+    if (pgpKeyTarget) {
+      return servePGPKey(pgpKeyTarget, version, dependencies);
+    }
+
     // For each supported keys endpoint serve the keys
-    if (validKeysRoutes.includes(url.pathname)) {
+    if (validSSHKeyRoutes.includes(url.pathname)) {
       return serveKeys(url, version, dependencies);
     }
 
@@ -68,11 +90,11 @@ function serveKeys(
   version: string,
   dependencies: ServerDependencies,
 ) {
-  const { filterIncludesKey, parseParameters, keys } = dependencies;
+  const { filterIncludesKey, parseParameters, sshKeys } = dependencies;
 
   /** Parse query params into filters object and filter all public keys. */
   const filter = parseParameters(url);
-  const filteredKeys = keys.filter((key) => filterIncludesKey(filter, key));
+  const filteredKeys = sshKeys.filter((key) => filterIncludesKey(filter, key));
 
   /** Format the public keys in a suitable way for an authorized_keys file. */
   const responseData = filteredKeys
