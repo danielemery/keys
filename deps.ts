b210ef25ef69d9f047871e0eb672277d0307a23d
Daniel Emery <danielremery@gmail.com>
2024-08-17 17:54:00 +0200

    #46 Update Deno, sentry and deno std library

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
 export { z } from "https://deno.land/x/zod@v3.23.8/mod.ts";
 export { ZodError } from "https://deno.land/x/zod@v3.23.8/ZodError.ts";
-export * as Sentry from "https://deno.land/x/sentry@8.19.0/index.mjs";
+export * as Sentry from "https://deno.land/x/sentry@8.26.0/index.mjs";
