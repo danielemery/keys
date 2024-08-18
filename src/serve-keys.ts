d6a6a7de96b76a958dd0541cadae2621c5b43bed
Daniel Emery <danielremery@gmail.com>
2024-08-18 17:35:07 +0200

    #45 Support content types when serving ssh routes

--- src/serve-keys.ts
+++ src/serve-keys.ts
@@ -1,25 +1,36 @@
 import { STATUS_CODE, STATUS_TEXT } from "@std/http";
 import { ServerDependencies } from "./server.ts";
+import { ContentType } from "./content-types.ts";
 
 export function serveKeys(
   url: URL,
   version: string,
   dependencies: ServerDependencies,
+  contentType: ContentType,
 ) {
   const { filterIncludesKey, parseParameters, sshKeys } = dependencies;
 
   const filter = parseParameters(url);
   const filteredKeys = sshKeys.filter((key) => filterIncludesKey(filter, key));
 
-  const responseData = filteredKeys
-    .map((key) => `${key.key} ${key.user}@${key.name}`)
-    .join("\n");
+  switch (contentType) {
+    case "text/plain": {
+      const responseData = filteredKeys
+        .map((key) => `${key.key} ${key.user}@${key.name}`)
+        .join("\n");
 
-  return new Response(responseData, {
-    status: STATUS_CODE.OK,
-    statusText: STATUS_TEXT[STATUS_CODE.OK],
-    headers: {
-      "X-Keys-Version": version,
-    },
-  });
+      return new Response(responseData, {
+        status: STATUS_CODE.OK,
+        statusText: STATUS_TEXT[STATUS_CODE.OK],
+        headers: {
+          "X-Keys-Version": version,
+        },
+      });
+    }
+    default:
+      return new Response(undefined, {
+        status: STATUS_CODE.NotAcceptable,
+        statusText: STATUS_TEXT[STATUS_CODE.NotAcceptable],
+      });
+  }
 }
