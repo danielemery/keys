ac03348cee7bd4cfde0365b6dfcb982b168d2aca
Daniel Emery <danielremery@gmail.com>
2024-08-18 16:28:59 +0200

    #45 Add basic content negotiation helper

--- deps.ts
+++ deps.ts
@@ -1,3 +1,4 @@
 export { z } from "https://deno.land/x/zod@v3.23.8/mod.ts";
 export { ZodError } from "https://deno.land/x/zod@v3.23.8/ZodError.ts";
 export * as Sentry from "https://deno.land/x/sentry@8.26.0/index.mjs";
+export { Accepts } from "https://deno.land/x/accepts@2.1.1/mod.ts";
